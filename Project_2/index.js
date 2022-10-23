60ac1a431942a3d969403d463ccc4cc1bba4e56e
Kofi Junior Eshun <eshunkofi23@gmail.com>
2022-10-22 18:40:08 -0600

    Next Commit

--- Project_2/index.js
+++ Project_2/index.js
@@ -4,42 +4,42 @@ const dotenv = require("dotenv");
 dotenv.config({path: "../Project_2/.env" });
 const PORT = process.env.PORT;
 const bodyParser = require("body-parser");
-
-
- const card = {
-    "Question" : "",
-    "Answer": {},
-    "Hints/Notes":"",
-    "datetime":"datetimestamp"
- }
-
-const study_collection = {
-    "Cards" : [card],
-    "Author": "user",
-    "datetime": ""
-}
-
-const user = {
-    "Username" : "email@username",
-    "Password" : "password",
-    "first_name" : "",
-    "last_name": "",
-    "datetime": ""
-}
-
-const folder = {
-    "Name": "folder_name",
-    "Collections": [],
-    "Description": " Folder description",
-    "School":"",
-    "Class_name": "",
-    "Teacher" : "",
-    "Semester": "",
-    "Year" : "",
-    "datetime": ""
-}
-
 app.use(bodyParser.urlencoded({extended:true}), bodyParser.json());
+
+//  const card = {
+//     "Question" : "",
+//     "Answer": {},
+//     "Hints/Notes":"",
+//     "datetime":"datetimestamp"
+//  }
+
+// const study_collection = {
+//     "Cards" : [card],
+//     "Author": "user",
+//     "datetime": ""
+// }
+
+// const user = {
+//     "Username" : "email@username",
+//     "Password" : "password",
+//     "first_name" : "",
+//     "last_name": "",
+//     "datetime": ""
+// }
+
+// const folder = {
+//     "Name": "folder_name",
+//     "Collections": [],
+//     "Description": " Folder description",
+//     "School":"",
+//     "Class_name": "",
+//     "Teacher" : "",
+//     "Semester": "",
+//     "Year" : "",
+//     "datetime": ""
+// }
+
+
 const routes = require('./Backend/routes/user');
 app.use(routes);
 
