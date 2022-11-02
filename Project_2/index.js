b51b3f062a9c8ff9fd82c8c1d6769d4361abcbf7
Kofi Junior Eshun <eshunkofi23@gmail.com>
2022-11-01 19:20:27 -0600

    11/1/2022 Commit

--- Project_2/index.js
+++ Project_2/index.js
@@ -6,11 +6,14 @@ const ENVIRONMENT_VARIABLES = require("./Backend/encryption/dotenv");
 const PORT = ENVIRONMENT_VARIABLES.Database_PORT;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}), bodyParser.json());
+var cors = require('cors')
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const routes = require('./Backend/routes/user');
+app.use(cors());
 app.use(routes);
-exports.wss = wss = new WebSocket.Server({server:server});
+var wss = new WebSocket.Server({server:server});
+
 
 wss.on('connection', function connection(ws) {
     console.log("Connected!");
